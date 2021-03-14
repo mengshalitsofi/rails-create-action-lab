ae47af60ace82c4d790451ca8ce0414d0b4a3f8b
mengshalitsofi <mengshalitsofi@gmail.com>
2021-03-13 16:29:41 -0800

    commit tree fddc838d2e98f2c9e1df6efdfa778a4cd21b4d2f

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,19 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:first_name]
+    @student.save
+    redirect_to student_path(@student)
+
   end
 
+  # def student_params
+  #   params.require(:student).permit(first_name, last_name)
+  # end
 end
